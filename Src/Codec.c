91f7a938d95f327b05353c3984dc550f251e3097
Ali Mirghasemi <ali.mirghasemi1376@gmail.com>
2022-07-07 23:27:26 +0430

    Update Codec
    update codec
    tested
    add version control

--- Src/Codec.c
+++ Src/Codec.c
@@ -1,12 +1,30 @@
 #include "Codec.h"
 
-
+/**
+ * @brief initialize codec
+ *
+ * @param codec codec to initialize
+ * @param baseLayer base layer of codec
+ * @param frame frame to use for codec
+ */
 void Codec_init(Codec* codec, Codec_LayerImpl* baseLayer, Codec_Frame* frame) {
     codec->BaseLayer = baseLayer;
     codec->CurrentLayer = baseLayer;
     codec->Frame = frame;
+    codec->onFrame = (Codec_OnFrameFn) 0;
+#if CODEC_DECODE_ERROR || CODEC_ENCODE_ERROR
+    codec->onError = (Codec_OnErrorFn) 0;
+#endif
+#if CODEC_ARGS
+    codec->Args = (void*) 0;
+#endif
 }
-
+/**
+ * @brief decode frame over input stream
+ *
+ * @param codec codec to decode
+ * @param stream input stream to decode
+ */
 void Codec_decode(Codec* codec, IStream* stream) {
     Codec_LayerImpl* layer = codec->CurrentLayer;
     Codec_Frame* frame = codec->Frame;
@@ -14,10 +32,10 @@ void Codec_decode(Codec* codec, IStream* stream) {
     Stream_LenType layerLen;
 
     layerLen = layer->getLayerLen(codec, frame);
-    while (IStream_available(stream) > layerLen) {
+    while (IStream_available(stream) >= layerLen) {
         // set limit for read header part
         IStream_setLimit(stream, layerLen);
-        if ((error = layer->parse(codec, stream, frame)) != CODEC_OK) {
+        if ((error = layer->parse(codec, frame, stream)) != CODEC_OK) {
         #if CODEC_DECODE_ERROR
             if (codec->onError) {
                 codec->onError(codec, frame, layer, error);
@@ -38,20 +56,30 @@ void Codec_decode(Codec* codec, IStream* stream) {
                 layer = codec->BaseLayer;
             }
         }
+        // clear limit for read
+        IStream_setLimit(stream, STREAM_NO_LIMIT);
         // get layer len
         layerLen = layer->getLayerLen(codec, frame);
     }
     // update current layer under process
     codec->CurrentLayer = layer;
 }
-
-Codec_Error Codec_encode(Codec* codec, OStream* stream, Codec_Frame* frame) {
+/**
+ * @brief encode frame over output stream
+ *
+ * @param codec
+ * @param frame
+ * @param stream
+ * @param mode encode mode
+ * @return Codec_Error
+ */
+Codec_Error Codec_encode(Codec* codec, Codec_Frame* frame, OStream* stream, Codec_EncodeMode mode) {
     Codec_LayerImpl* layer = codec->BaseLayer;
     Codec_Error error;
 
     while (layer != CODEC_LAYER_NULL) {
-        OStream_setLimit(layer->getLayerLen(codec, frame));
-        if((error = layer->write(codec, stream, frame)) != CODEC_OK) {
+        OStream_setLimit(stream, layer->getLayerLen(codec, frame));
+        if((error = layer->write(codec, frame, stream)) != CODEC_OK) {
         #if CODEC_ENCODE_ERROR
             if (codec->onError) {
                 codec->onError(codec, frame, layer, error);
@@ -66,41 +94,58 @@ Codec_Error Codec_encode(Codec* codec, OStream* stream, Codec_Frame* frame) {
             OStream_writePadding(stream, CODEC_LAYER_PADDING_VALUE, OStream_getLimit(stream));
         #endif
     #endif
+        if (Codec_EncodeMode_FlushLayer == mode) {
+            OStream_flush(stream);
+        }
         layer = layer->getUpperLayer(codec, frame);
     }
-    
+
     OStream_setLimit(stream, STREAM_NO_LIMIT);
 
-    return error;
-}
-
-Codec_Error Codec_encodeFlush(Codec* codec, OStream* stream, Codec_Frame* frame) {
-    Codec_LayerImpl* layer = codec->BaseLayer;
-    Codec_Error error;
-
-    while (layer != CODEC_LAYER_NULL) {
-        OStream_setLimit(layer->getLayerLen(codec, frame));
-        if((error = layer->write(codec, stream, frame)) != CODEC_OK) {
-        #if CODEC_ENCODE_ERROR
-            if (codec->onError) {
-                codec->onError(codec, frame, layer, error);
-            }
-        #endif
-            break;
-        }
-    #if CODEC_LAYER_PADDING
-        #if CODEC_LAYER_PADDING_MODE == CODEC_LAYER_PADDING_IGNORE
-            OStream_ignore(stream, OStream_getLimit(stream));
-        #else
-            OStream_writePadding(stream, CODEC_LAYER_PADDING_VALUE, OStream_getLimit(stream));
-        #endif
-    #endif
+    if (error == CODEC_OK && Codec_EncodeMode_Flush == mode) {
         OStream_flush(stream);
-        layer = layer->getUpperLayer(codec, frame);
     }
-    
-    OStream_setLimit(stream, STREAM_NO_LIMIT);
 
     return error;
 }
+/**
+ * @brief set on decode frame callback
+ *
+ * @param codec
+ * @param fn
+ */
+void Codec_onFrame(Codec* codec, Codec_OnFrameFn fn) {
+    codec->onFrame = fn;
+}
+#if CODEC_ENCODE_ERROR
+/**
+ * @brief set on encode/decode error callback
+ *
+ * @param codec
+ * @param fn
+ */
+void Codec_onError(Codec* codec, Codec_OnErrorFn fn) {
+    codec->onError = fn;
+}
+#endif
 
+#if CODEC_ARGS
+/**
+ * @brief set user args
+ *
+ * @param codec
+ * @param args
+ */
+void Codec_setArgs(Codec* codec, void* args) {
+    codec->Args = args;
+}
+/**
+ * @brief get user args
+ *
+ * @param codec
+ * @return void*
+ */
+void* Codec_getArgs(Codec* codec) {
+    return codec->Args;
+}
+#endif
--- Src/Codec.h
+++ Src/Codec.h
@@ -4,9 +4,9 @@
  * @brief this library help you for encode/decode frames, it's based on stream library
  * @version 0.1
  * @date 2021-09-01
- * 
+ *
  * @copyright Copyright (c) 2021
- * 
+ *
  */
 
 #ifndef _CODEC_H_
@@ -19,14 +19,24 @@ extern "C" {
 #include "InputStream.h"
 #include "OutputStream.h"
 
+#define CODEC_VER_MAJOR    0
+#define CODEC_VER_MINOR    1
+#define CODEC_VER_FIX      0
+
 /************************************************************************/
 /*                            Configuration                             */
 /************************************************************************/
-
+/**
+ * @brief enable user arguments into codec
+ */
 #define CODEC_ARGS                          1
-
+/**
+ * @brief enable auto padding for fix layer sizes
+ */
 #define CODEC_LAYER_PADDING                 1
-
+/**
+ * @brief select padding mode
+ */
 #if CODEC_LAYER_PADDING
     #define CODEC_LAYER_PADDING_IGNORE      0
     #define CODEC_LAYER_PADDING_VALUE       0xFF
@@ -34,43 +44,104 @@ extern "C" {
     #define CODEC_LAYER_PADDING_MODE        CODEC_LAYER_PADDING_IGNORE
 
 #endif
-
+/**
+ * @brief enable encode error callback
+ */
 #define CODEC_ENCODE_ERROR                  1
-
+/**
+ * @brief enable decode error callback
+ */
 #define CODEC_DECODE_ERROR                  1
-
+/**
+ * @brief choose what type use for codec frame, default is void
+ */
 typedef void Codec_Frame;
-
+/**
+ * @brief choose what type use for codec error
+ */
 typedef uint32_t Codec_Error;
-
+/**
+ * @brief choose what type use for codec layer index
+ */
 typedef uint16_t Codec_LayerIndex;
 
 /************************************************************************/
+#if !STREAM_WRITE_LIMIT
+    #error "Codec Library use STREAM_LIMIT_WRITE, you must enable it"
+#endif
 
+#if !STREAM_READ_LIMIT
+    #error "Codec Library use STREAM_LIMIT_READ, you must enable it"
+#endif
+
+#define __CODEC_VER_STR(major, minor, fix)     #major "." #minor "." #fix
+#define _CODEC_VER_STR(major, minor, fix)      __CODEC_VER_STR(major, minor, fix)
+/**
+ * @brief show codec version in string format
+ */
+#define CODEC_VER_STR                          _CODEC_VER_STR(CODEC_VER_MAJOR, CODEC_VER_MINOR, CODEC_VER_FIX)
+/**
+ * @brief show codec version in integer format, ex: 0.2.0 -> 200
+ */
+#define CODEC_VER                              ((CODEC_VER_MAJOR * 10000UL) + (CODEC_VER_MINOR * 100UL) + (CODEC_VER_FIX))
+/**
+ * @brief return ok of parse was successful
+ */
 #define CODEC_OK                ((Codec_Error) 0)
-
+/**
+ * @brief return null when it's last layer
+ */
 #define CODEC_LAYER_NULL        ((Codec_LayerImpl*) 0)
 
+/* Pre-define data types */
 struct __Codec;
 typedef struct __Codec Codec;
-
 struct __Codec_LayerImpl;
 typedef struct __Codec_LayerImpl Codec_LayerImpl;
-
-typedef Codec_Error (*Codec_ParseFn)(Codec* codec, IStream* stream, Codec_Frame* frame);
-typedef Codec_Error (*Codec_WriteFn)(Codec* codec, OStream* stream, Codec_Frame* frame);
+/**
+ * @brief this function parse layer from input stream
+ */
+typedef Codec_Error (*Codec_ParseFn)(Codec* codec, Codec_Frame* frame, IStream* stream);
+/**
+ * @brief this function write layer into output stream
+ */
+typedef Codec_Error (*Codec_WriteFn)(Codec* codec, Codec_Frame* frame, OStream* stream);
+/**
+ * @brief this function return size of layer in bytes
+ */
 typedef Stream_LenType (*Codec_GetLenFn)(Codec* codec, Codec_Frame* frame);
+/**
+ * @brief this function return next or upper layer of packet, return null if it's last layer
+ */
 typedef Codec_LayerImpl* (*Codec_GetUpperLayerFn)(Codec* codec, Codec_Frame* frame);
+/**
+ * @brief this function is called when codec decode a frame from stream
+ */
 typedef void (*Codec_OnFrameFn)(Codec* codec, Codec_Frame* frame);
+/**
+ * @brief this function is called when encode/decode error occurred
+ */
 typedef void (*Codec_OnErrorFn)(Codec* codec, Codec_Frame* frame, Codec_LayerImpl* layer, Codec_Error error);
-
+/**
+ * @brief codec encode modes
+ */
+typedef enum {
+  Codec_EncodeMode_Normal       = 0x00,         /**< normal encode mode, just write to stream */
+  Codec_EncodeMode_Flush        = 0x01,         /**< flush stream after encode done */
+  Codec_EncodeMode_FlushLayer   = 0x03,         /**< flush each layer */
+} Codec_EncodeMode;
+/**
+ * @brief hold layer implementation
+ */
 struct __Codec_LayerImpl {
     Codec_ParseFn           parse;
     Codec_WriteFn           write;
     Codec_GetLenFn          getLayerLen;
     Codec_GetUpperLayerFn   getUpperLayer;
 };
-
+/**
+ * @brief hold codec parameters
+ */
 struct __Codec {
 #if CODEC_ARGS
     void*                   Args;
@@ -84,7 +155,10 @@ struct __Codec {
 
 void Codec_init(Codec* codec, Codec_LayerImpl* baseLayer, Codec_Frame* frame);
 void Codec_onFrame(Codec* codec, Codec_OnFrameFn fn);
-void Codec_onError(Codec* codec, Codec_OnErrorFn fn);
+
+#if CODEC_ENCODE_ERROR || CODEC_DECODE_ERROR
+    void Codec_onError(Codec* codec, Codec_OnErrorFn fn);
+#endif 
 
 #if CODEC_ARGS
     void  Codec_setArgs(Codec* codec, void* args);
@@ -92,8 +166,7 @@ void Codec_onError(Codec* codec, Codec_OnErrorFn fn);
 #endif
 
 void Codec_decode(Codec* codec, IStream* stream);
-Codec_Error Codec_encode(Codec* codec, OStream* stream, Codec_Frame* frame);
-Codec_Error Codec_encodeFlush(Codec* codec, OStream* stream, Codec_Frame* frame);
+Codec_Error Codec_encode(Codec* codec, Codec_Frame* frame, OStream* stream, Codec_EncodeMode mode);
 
 
 #ifdef __cplusplus
